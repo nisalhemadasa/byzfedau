866e6ee2b658513ef979aa818ff49666ea2c508b
Rubén <rubgij@gmail.com>
2025-07-15 09:58:55 +0200

    Update

--- tests/test_efficacy.py
+++ tests/test_efficacy.py
@@ -5,20 +5,22 @@ import torch.nn.functional as F
 
 from src.utils import evaluate_efficacy
 
+
 class DummyModel(nn.Module):
     def __init__(self):
         super().__init__()
-    
+
     def forward(self, x):
         """If input ends with 2 returns 0 else 1"""
         return F.one_hot((x % 10 != 2).long(), num_classes=2)
+
 
 def test_efficacy():
     """Dummy test for efficacy evaluation also knonw as ASR (Attackt success rate)."""
     n = 10_000
     x = torch.arange(n)
     y = torch.ones(n, dtype=torch.long)
-    
+
     dataset = TensorDataset(x, y)
     dataloader = DataLoader(dataset, batch_size=128)
 
@@ -27,6 +29,7 @@ def test_efficacy():
     if efficacy != 0.9:
         raise ValueError(f"Efficacy should be 0.9. Value: {efficacy}")
     print("all test passed :)")
+
 
 if __name__ == "__main__":
     test_efficacy()
