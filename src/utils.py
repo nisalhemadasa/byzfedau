dea751d0b7ed6d93087dfe61156dc150385bf019
Rubén <rubgij@gmail.com>
2025-07-15 10:33:36 +0200

    Bug fixed

--- src/utils.py
+++ src/utils.py
@@ -55,7 +55,7 @@ class BackdoorCrossStamp:
         If there is enough time should be vetorized.
         """
         return torch.stack([
-            self.stamp(img for img in imgs)
+            self.stamp(img) for img in imgs
         ])
         
 
