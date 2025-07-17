b4e0d3447b2e344868c44f133a74c88aabf34c5d
Rubén <74114026+rubzip@users.noreply.github.com>
2025-07-17 12:14:26 +0200

    shufflenet

--- src/task.py
+++ src/task.py
@@ -18,32 +18,34 @@ from torchvision import models
 from src.attacks import flip_sign, add_gaussian_noise, flip_labels
 from src.utils import BackdoorCrossStamp
 
-class Net(nn.Module):
-    """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
+# class Net(nn.Module):
+#     """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
 
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.bn1 = nn.BatchNorm2d(6)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.bn3 = nn.BatchNorm1d(120)
-        self.fc2 = nn.Linear(120, 84)
-        self.bn4 = nn.BatchNorm1d(84)
-        self.fc3 = nn.Linear(84, 10)
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.bn1 = nn.BatchNorm2d(6)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.bn2 = nn.BatchNorm2d(16)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.bn3 = nn.BatchNorm1d(120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.bn4 = nn.BatchNorm1d(84)
+#         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = x.view(-1, 16 * 5 * 5)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         return self.fc3(x)
 
-# def Net():
-#     return models.resnet18(pretrained=False)
+def Net():
+    model = models.shufflenet_v2_x1_0(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, 10)
+    return model
 
 
 fds = None  # Cache FederatedDataset
